1747a575b5b451872ae0ee5b579aa040765dce98
Vinicius Lima <vini-toninho@hotmail.com>
2021-03-25 12:58:52 -0300

    Adiconado Desafio 1 e 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let pets  = [ {
     peso: 5,
     tutor: 'vinicius',
     contato: '(81)98529-1481',
-    vacinado: true,
+    vacinado: false,
     servicos: ['banho', 'tosa']
 
 },
@@ -25,33 +25,74 @@ let pets  = [ {
 
 },
 {
-    nome: 'joey Jr',
+    nome: 'joeyJr',
     tipo: 'coelho',
     idade: 1,
     raca: 'Raraça Pura',
     peso: 2,
     tutor: 'Pierre',
     contato: '(81)99999-9999',
-    vacinado: true,
+    vacinado: false,
     servicos: ['vacina', 'castração']
 
 }
 ];
 
+/*
 const listarpets = () => {
 
     for(let pet of pets){
         console.log(`O nome do pet é:  ${pet.nome}`);
     }
     
-    /*
+    
     for(let i = 0; i < pet.length; i++){
 
-        console.log('O nome do pet é: ' + pets[i].nome);
-        console.log(`O nome do pet é:  ${pets[i].nome}`);
+        console.log('O nome do pet é: ' + pet[i].nome);
+        console.log(`O nome do pet é:  ${pet[i].nome}`);
     }
-    */
+    
 }
 
 listarpets();
+
+*/
+
+const VerificaVacinaPet = (animal) => {
+   
+    for(let pet of pets){
+        if(animal == pet.nome){
+            if(pet.vacinado == false){
+                pet.vacinado = true;
+                console.log(`Pet ${pet.nome} não era vacinado e agora esta vacinado`);
+                
+        }
+        else{
+            console.log(`Pet ${pet.nome} ja era vacinado`);
+            
+        }
+        
+        
+    }
+
+    
+   }
+        
+};
+//VerificaVacinaPet('bolota');
+
+const campanhaVacina = () =>{
+    let cont = 0;
+    for(let pet of pets){
+        
+        if(pet.vacinado == false){
+            pet.vacinado = true;
+            cont = cont + 1;
+        }
+    
+   }
+   console.log(`Dentre todos os ${pets.length} pets, ${cont} foram vacinados!!!`);
+
+}
+campanhaVacina();
 
